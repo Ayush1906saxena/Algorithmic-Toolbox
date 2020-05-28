c97d1a38e1e10386e2cc1a6faf4c2059d0c6e22d
Ayush Saxena <64210313+Ayush1906saxena@users.noreply.github.com>
2020-05-28 21:00:50 +0530

    Update longest_common_2.cpp

--- Week 5/longest_common_2.cpp
+++ Week 5/longest_common_2.cpp
@@ -36,51 +36,41 @@
 
 typedef long long int ll;
 
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+
 using namespace std;
 
-int LCS(vector<int> _One, vector<int> _Second, vector<int> _third, int a, int b, int c) {
-	vector<vector<vector<int>>> Longest(a + 1,vector<vector<int>>(b + 1,vector<int>(c + 1)));
+int LCS(string _One, string _Second, int m, int n) {
+	vector<vector<int>> Longest(m + 1,vector<int>(n + 1));
+	int i, j;
+	for (i = 0; i <= m; i++){
+		for (j = 0; j <= n; j++){
+			if (i == 0 || j == 0)
+				Longest[i][j] = 0;
 
-	for (int i = 0; i <= a; i++) {
-		for (int j = 0; j <= b; j++) {
-			for (int k = 0; k <= c; k++) {
-				if (i == 0 || j == 0 || k == 0) {
-					Longest[i][j][k] = 0;
-					continue;
-				}
-				else if (_One[i - 1] == _Second[j - 1] && _One[i - 1] == _third[k - 1]) {
-					Longest[i][j][k] = Longest[i - 1][j - 1][k - 1] + 1;
-					continue;
-				}
-				else {
-					Longest[i][j][k] =
-						max( max(Longest[i - 1][j][k], Longest[i][j - 1][k]),
-							Longest[i][j][k - 1]);
-				}
-			}
+			else if (_One[i - 1] == _Second[j - 1])
+				Longest[i][j] = Longest[i - 1][j - 1] + 1;
 
+			else
+				Longest[i][j] = max(Longest[i - 1][j], Longest[i][j - 1]);
 		}
 	}
-	return Longest[a][b][c];
+	return Longest[m][n];
 }
-
 int main() {
-
-	int a, b, c;
-	cin >> a;
-	vector<int> One(a);
-	for (int i = 0; i < a; i++)
-		cin >> One[i];
-
-	cin >> b;
-	vector<int> Second(b);
-	for (int i = 0; i < b; i++) 
-		cin >> Second[i];
-
-	cin >> c;
-	vector<int> Third(c);
-	for (int i = 0; i < c; i++)
-		cin >> Third[i];
-
-	cout << LCS(One, Second, Third, a, b, c) << endl;
-} 
+	int m, n;
+	string _One = "", _Second = "";
+	cin >> m;
+	int Tempo;
+	for (size_t i = 0; i < m; i++) {
+		cin >> Tempo;
+		_One += Tempo;
+	}
+	cin >> n;
+	for (size_t i = 0; i < n; i++) {
+		cin >> Tempo;
+		_Second += Tempo;
+	}
+	cout << LCS(_One, _Second, m, n) << endl;
+}
